940c262507a361ace3d77e62ed387c6c77914ac5
aldse <eliana.tete1211@gmail.com>
2024-01-24 16:06:20 -0300

    decimo 7 commit

--- FormularioJogo.cs
+++ FormularioJogo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -5,25 +6,105 @@ namespace JogoWinforms
 {
     public class FormularioJogo : Form
     {
-        private Image img = null;
-        private Bitmap bmp = null;
+        public PictureBox PictureBox { get; set; }
+        public Graphics Graphics { get; set; }
+        private FlowLayoutPanel acima;
+        private FlowLayoutPanel debaixo;
 
         public FormularioJogo()
         {
+            PictureBox = new PictureBox();
+            PictureBox.Width = 800;
+            PictureBox.Height = 600;
+
             Width = Screen.PrimaryScreen.Bounds.Width / 2;
             Height = Screen.PrimaryScreen.Bounds.Height / 2;
             StartPosition = FormStartPosition.CenterScreen;
             FormBorderStyle = FormBorderStyle.None;
 
-            BackColor = Color.Plum;
-            // // Carrega a imagem do arquivo
-            // img = Image.FromFile("img/fundomenu.png");
+            acima = new FlowLayoutPanel();
+            acima.Dock = DockStyle.Fill;
+            acima.AutoScroll = true;
 
-            // // Define a imagem como fundo do formulário
-            // BackgroundImage = img;
+            Controls.Add(acima);
 
-            // // Ajusta o layout para cobrir a área do formulário
-            // BackgroundImageLayout = ImageLayout.Stretch;
+            Card("Card 1");
+            Card("Card 3");
+            Card("Card 5");
+            Card("Card 7");
+            Card("Card 11");
+            Card("Card 20");
+            Card("Card 24");
+            Card("Card 28");
+            Card("Card 31");
+            Card("Card 35");
+            Card("Card 39");
+            Card("Card 47");
+
+            debaixo = new FlowLayoutPanel();
+            debaixo.Dock = DockStyle.Bottom;
+            debaixo.Height = 120; 
+
+            Controls.Add(debaixo);
+
+            CardInferior("1");
+            CardInferior("2");
+            CardInferior("3");
+            CardInferior("4");
+
+            Botao();
         }
+
+        private void Card(string texto)
+        {
+            var card = new UserControl();
+            card.Size = new System.Drawing.Size(150, 100);
+            card.BorderStyle = BorderStyle.FixedSingle;
+
+            var label = new Label();
+            label.Text = texto;
+            card.Controls.Add(label);
+
+            acima.Controls.Add(card);
+        }
+
+        private void CardInferior(string texto)
+        {
+            var card = new UserControl();
+            card.Size = new System.Drawing.Size(150, 100);
+            card.BorderStyle = BorderStyle.FixedSingle;
+
+            var label = new Label();
+            label.Text = texto;
+            card.Controls.Add(label);
+
+            debaixo.Controls.Add(card);
+        }
+
+
+        public void Botao()
+        {
+            Button sair = new Button();
+            sair.FlatStyle = FlatStyle.Flat;
+            sair.FlatAppearance.BorderSize = 0;
+            sair.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            sair.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            sair.BackColor = Color.Black; 
+            sair.ForeColor = Color.Red;
+            sair.Font = new Font("Arial", 12);
+            sair.Text = "Menu";
+            sair.Size = new Size(200, 30);
+            sair.Width = 230;
+            sair.Height = 85;
+            sair.Location = new Point(PictureBox.Width - 10, PictureBox.Height - 80);
+
+            sair.Click += delegate
+            {
+                this.Close();
+            };
+
+            Controls.Add(sair);
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# JogoWinforms
+#Roubadas do Jogo
+
+Pista Fugaz: Dar uma dica de qual caminho seguir, justo o suficiente para traçar a olinha que aparece, mas desaparece em 3 segundos.
+
+Travessia Dupla: Atravessar duas linhas traçadas.
+
+Inversão de Destino: Mudar a posição das bolinhas.
+
+Túnel Residencial: Criar um túnel, onde a bolinha, ao estar com uma casa na frente dela e m parceiro da mesma cor com uma casa livre na frente, conecta-se.
+
+Rota Labiríntica: Atravessar de um lado para outro na mesma linha quando sai da tela, riando uma rota labiríntica.
+
+Fuga Instantânea: Atravessar de um lado para outro quando sai da tela, atravessando uma inha já traçada.
+
+Dança das Bolinhas: Dois pares de bolinhas mudam de lugar.
+
+Aniquilação Tática: Apagar um par de bolinhas.
+
+Imigrante: Mudar uma bolinha de lugar.
+
+Linha Invisível: Atravessar 1 linha já traçada.
